1a3dc0f7c9341ba1e3e69f0237b3a1d530f34a22
mgrcar <miha.grcar@ijs.si>
2015-12-26 20:08:48 +0100

    Added empty MVC project

---
+++ MvcProject/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Optimization;
+
+namespace EmptyMvcProject
+{
+    public class BundleConfig 
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear(); // allow adding minified js files
+            BundleTable.EnableOptimizations = true;
+            // Styles
+            bundles.Add(new StyleBundle("~/css/bootstrap").Include(
+                "~/css/bootstrap.css"
+                ));
+            // Scripts
+            bundles.Add(new ScriptBundle("~/js/jquery").Include(
+                "~/js/jquery-2.1.0.min.js"
+                ));
+            bundles.Add(new ScriptBundle("~/js/bootstrap").Include(
+                "~/js/bootstrap.js"
+                ));
+        }
+    }
+}
