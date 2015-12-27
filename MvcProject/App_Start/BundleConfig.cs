00ccacfc9b3fad1159bff08d302a05437234808d
mgrcar <miha.grcar@ijs.si>
2015-12-27 10:25:34 +0100

    Created CSS/JS bundles, removed obsolete code

--- MvcProject/App_Start/BundleConfig.cs
+++ MvcProject/App_Start/BundleConfig.cs
@@ -7,18 +7,32 @@ namespace DocsTemplate
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.IgnoreList.Clear(); // allow adding minified js files
-            BundleTable.EnableOptimizations = true;
+            //BundleTable.EnableOptimizations = true; 
             // Styles
-            bundles.Add(new StyleBundle("~/css/bootstrap").Include(
-                "~/css/bootstrap.css"
-                ));
+            bundles.Add(new StyleBundle("~/css/bootstrap-docs").Include(
+                "~/css/bootstrap.min.css",
+                "~/css/docs.min.css"
+            ));
+            bundles.Add(new StyleBundle("~/css/prism").Include(
+                "~/css/prism.min.css"
+            ));
+            bundles.Add(new StyleBundle("~/css/custom").Include(
+                "~/css/custom.css"
+            ));
             // Scripts
             bundles.Add(new ScriptBundle("~/js/jquery").Include(
-                "~/js/jquery-2.1.0.min.js"
-                ));
-            bundles.Add(new ScriptBundle("~/js/bootstrap").Include(
-                "~/js/bootstrap.js"
-                ));
+                "~/js/jquery.min.js"
+            ));
+            bundles.Add(new ScriptBundle("~/js/bootstrap-docs").Include(
+                "~/js/bootstrap.min.js",
+                "~/js/docs.min.js"
+            ));
+            bundles.Add(new ScriptBundle("~/js/prism").Include(
+                "~/js/prism.min.js"
+            ));
+            bundles.Add(new ScriptBundle("~/js/hacks").Include(
+                "~/js/ie10-viewport-bug-workaround.js"
+            ));
         }
     }
 }
